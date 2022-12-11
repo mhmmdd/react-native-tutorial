b5ae02669c5c6632760d205a766b9e9573c2c8f7
mhmmdd <mhmmd.dd@gmail.com>
2022-12-11 23:09:43 +0300

    01 - Changes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  Image,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableHighlight
+} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
+  console.log(require('./assets/icon.png'));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // SafeAreaView is working only IOS
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.tsx to start working on your app! - A really really long text.
+        Now I wanna make this even
+        longer and see what happens!</Text>
+
+      {/*Local Image*/}
+      {/*<Image source={require('./assets/icon.png')}/>*/}
+
+      {/*TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight*/}
+      <TouchableHighlight onPress={() => console.log("Image tapped")}>
+        {/*Network Image*/}
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }}/>
+      </TouchableHighlight>
+
+      <Button title={"Click Me"} onPress={() => alert("Button tapped")}/>
+
+      <Button title={"Customized Alert"}
+              onPress={() =>
+                Alert.alert('My title', 'My message', [
+                  {text: "Btn1", onPress: () => console.log('Btn1 tapped')},
+                  {text: "Btn2", onPress: () => console.log('Btn2 tapped')},
+                  {text: "Btn3", onPress: () => console.log('Btn3 tapped')},
+                ])}/>
+
+    </SafeAreaView>
   );
 }
 
+// Validates and optimizes the style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## 01 - Changes
+1. SafeAreaView: It is working only IOS
+2. Text: numberOfLines, onPress
+3. Image: local and network image
+4. TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight
+5. Button: Customized alert
+6. Platform specific style: Platform.OS === "android" ? StatusBar.currentHeight : 0,
